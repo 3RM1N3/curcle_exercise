d5ad1da84bac94654fd8b4a7387451949060a9ee
WiardYu <47353218+WiardYu@users.noreply.github.com>
2020-05-19 11:03:08 +0800

    Update funcs.py

--- funcs.py
+++ funcs.py
@@ -1,7 +1,20 @@
 def import_data(path):
     '''通过传入路径导入题库'''
+    
+    # 打开文件
+    with open('path', 'r', encoding='UTF-8') as f:
+        lines = f.readlines()
 
-    pass
+    '''
+    定义全部题目嵌套列表，数据结构:
+    all_questions = [['题目1', {'A': '选项', 'B': '选项', 'C': '选项', 'D': '选项', 'E': '选项'}, '正确答案'],
+                     ['题目2', {'A': '选项', 'B': '选项', 'C': '选项', 'D': '选项', 'E': '选项'}, '正确答案'],
+                     ...]
+    '''
+    all_questions = []
+    for l in lines:
+        pass
+
 
 
 def generate_question_dict():
@@ -11,4 +24,4 @@ def generate_question_dict():
 
 
 if __name__ == "__main__":
-    print('hello')
+    pass
