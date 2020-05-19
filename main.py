98eece6aaa591fbe2b6634dc1c8082ebd0d9a2bd
WiardYu <47353218+WiardYu@users.noreply.github.com>
2020-05-19 15:36:54 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from gui import *
-from funcs import *
+import gui
+import funcs
+import global_var
 
 
 if __name__ == "__main__":
-
-    dict1 = {'A': '甲', 'B': '乙', 'C': '丙', 'D': '丁', 'E': '戊'}
-
-    top.mainloop()
+    global_var.count = 0
+    global_var.ques_list = funcs.import_data('病理选择15章.txt')
+    gui.label_1['text'] = global_var.ques_list[global_var.count][0]
+    funcs.generate_options(global_var.ques_list[global_var.count][1])
+    gui.top.mainloop()
