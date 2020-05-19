b731cf0b6c974d7023a05a7172f41751a2b3ea0c
WiardYu <47353218+WiardYu@users.noreply.github.com>
2020-05-19 08:10:55 +0800

    update

--- main.py
+++ main.py
@@ -4,12 +4,5 @@ from ui import *
 if __name__ == "__main__":
 
     dict1 = {'A': '甲', 'B': '乙', 'C': '丙', 'D': '丁', 'E': '戊'}
-    radio_bt(dict1)
-
-    # 放置组件
-    label_1.grid(column=1, row=1, sticky='NSWE', padx=2, pady=2)
-    button_pre.grid(row=1, column=1, pady=2)
-    button_next.grid(row=1, column=2, padx=30, pady=2)
-    button_sub.grid(row=1, column=3, pady=2)
-
+    
     top.mainloop()
--- ui.py
+++ ui.py
@@ -1,11 +1,16 @@
 import tkinter as tk
+import time
 import tkinter.font as tkFont
 
+
+def test():
+    label_1['text'] = 'hahaha'
 
 # 定义窗口
 top = tk.Tk()
 top.title('小圆练题')
 top.resizable(0, 0)    # 不可改变窗口大小
+
 
 # 定义 & 放置框架
 f1 = tk.Frame(top)
@@ -15,12 +20,12 @@ f1.grid(row=1, pady=3)
 f2.grid(row=2, pady=3)
 f3.grid(row=3, pady=3)
 
+
 # 定义可变文本
 choosen = tk.StringVar()        # 用于获取已选择项
 
 
-# 定义标签显示题目
-
+# 定义 & 放置标签显示题目
 label_1 = tk.Label(
     f1,
     text = '',
@@ -32,10 +37,10 @@ label_1 = tk.Label(
     height = 8,
     width = 60,
 )
+label_1.grid(column=1, row=1, sticky='NSWE', padx=2, pady=2)
 
 
-
-# 定义单选
+# 定义 & 放置单选
 radio_bt_A = tk.Radiobutton(
     f2,
     text = '',
@@ -94,30 +99,26 @@ button_pre = tk.Button(
     text = '提交，到上一题',
     width = 15
 )
-
 # 定义下一题按钮
 button_next = tk.Button(
     f3,
     text = '提交，到下一题',
     width = 15
 )
-
 # 定义交卷按钮
 button_sub = tk.Button(
     f3,
     text = '现在交卷',
-    width = 15
+    width = 15,
 )
+button_pre.grid(row=1, column=1, pady=2)
+button_next.grid(row=1, column=2, padx=40, pady=2)
+button_sub.grid(row=1, column=3, pady=2)
 
 
 if __name__ == "__main__":
     
     label_1['text'] = ('1.某男，60岁，纤维胃镜检查发现胃窦部有一直径3cm大小边缘高起的溃疡，你认为确诊的方法是')
     options_dict = {'A': '细胞学检查', 'B': '活体组织检查', 'C': 'X线钡餐检查', 'D': '大便化验', 'E': '胃液分析'}
-    # 放置组件
-    label_1.grid(column=1, row=1, sticky='NSWE', padx=2, pady=2)
-    button_pre.grid(row=1, column=1, pady=2)
-    button_next.grid(row=1, column=2, padx=40, pady=2)
-    button_sub.grid(row=1, column=3, pady=2)
-
+    
     top.mainloop()
